a91ef346f40a26b77258b46067955abf3ddb5fbb
Tatenda07 <54758370+Tatenda07@users.noreply.github.com>
2020-03-16 11:44:28 +0800

    deply on gh-pages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,15 @@
 (function($) {
-  
-  "use strict";  
+
+  "use strict";
 
   $(window).on('load', function() {
 
-    /* 
+    /*
    MixitUp
    ========================================================================== */
   $('#portfolio').mixItUp();
 
-  /* 
+  /*
    One Page Navigation & wow js
    ========================================================================== */
     var OnePNav = $('.onepage-nev');
@@ -18,7 +18,7 @@
       currentClass: 'active',
       scrollOffset: top_offset,
     });
-  
+
   /*Page Loader active
     ========================================================*/
     $('#preloader').fadeOut();
@@ -52,7 +52,7 @@
 
     wow.init();
 
-    /* Nivo Lightbox 
+    /* Nivo Lightbox
     ========================================================*/
     $('.lightbox').nivoLightbox({
         effect: 'fadeScale',
@@ -89,6 +89,6 @@
 
 
 
-  });      
+  });
 
 }(jQuery));
